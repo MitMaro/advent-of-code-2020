d6e9fe5cb843a81a339d114c2d0fc502902cdc21
Tim Oram <dev@mitmaro.ca>
2020-12-01 20:34:59 -0330

    Initial project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Advent of Code - 2020
+
+Solutions to the [Advent of Code 2020](https://adventofcode.com/2020) written in Rust using [cargo-aoc](https://github.com/gobanos/cargo-aoc).
+
+## Instructions
+
+* Install [cargo-aoc](https://github.com/gobanos/cargo-aoc) with `cargo install cargo-aoc`
+* Run problem sets with `cargo-aoc` command
+
+## Solutions
+
+* [Day 1](src/day1.rs)
+
+## License
+
+This project is released under the ISC license. See [LICENSE](LICENSE).
---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// Make rustc's built-in lints more strict and set clippy into a whitelist-based configuration
+#![deny(
+	warnings,
+	nonstandard_style,
+	unused,
+	future_incompatible,
+	rust_2018_idioms,
+	unsafe_code
+)]
+#![deny(clippy::all, clippy::cargo, clippy::nursery, clippy::pedantic, clippy::restriction)]
+#![allow(clippy::blanket_clippy_restriction_lints)]
+
+extern crate aoc_runner;
+#[macro_use]
+extern crate aoc_runner_derive;
+
+aoc_lib! { year = 2020 }
